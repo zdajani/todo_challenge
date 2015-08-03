718729488567000d58f1339b429a9d06f38ad9cf
Zeina Dajani <dajani.zeina@gmail.com>
2015-08-03 01:13:31 +0100

    protractor now running on express server

--- test/e2e/conf.js
+++ test/e2e/conf.js
@@ -1,5 +1,5 @@
 exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  baseUrl: 'http://localhost:3000/index.html',
   specs: ['toDoListFeature.js'],
   capabilities: {
     'browserName': 'firefox'
